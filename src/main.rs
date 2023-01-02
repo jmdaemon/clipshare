8db4b374e74b7a1f0727ebc41635962a200bdfbd
Joseph Diza <josephm.diza@gmail.com>
2023-01-02 15:31:04 -0800

    Add rust log lib

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use clipboard::ClipboardProvider;
 use clipboard::ClipboardContext;
 use serde::{Serialize, Deserialize};
 
+extern crate pretty_env_logger;
+#[macro_use] extern crate log;
+
 // Data Structures
 pub struct Device {
     name: String,
@@ -17,7 +20,7 @@ type Shortcuts = HashMap<String, String>;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     max_history: u64,
-    //shortcuts: Shortcuts,
+    shortcuts: Shortcuts,
 }
 
 
@@ -28,28 +31,26 @@ pub fn get_clipboard_conts() -> String {
 }
 
 
-//impl Config {
-    //pub fn default(&self) -> Config {
-        //let shortcuts = HashMap::from([
-                //(String::from("Enable/Disable Device"), String::from("Ctrl + {}")),
-        //]);
+impl Config {
+    pub fn default() -> Config {
+        let shortcuts = HashMap::from([
+                (String::from("Enable/Disable Device"), String::from("Ctrl + {}")),
+        ]);
         //self.new(10_000, shortcuts)
-    //}
-    //pub fn new(&self, max_history: u64, shortcuts: Shortcuts) -> Config {
-        ////Config { max_history, shortcuts }
+        Config { max_history:10_000, shortcuts}
+    }
+    pub fn new(&self, max_history: u64, shortcuts: Shortcuts) -> Config {
+        Config { max_history, shortcuts }
         //Config { max_history }
-    //}
-//}
+    }
+}
 
 fn main() {
+    pretty_env_logger::init();
 
-    // Init
-    //let cfg = Config::default;
-    let cfg = Config { max_history: 10_000 };
-
+    let cfg = Config::default();
     let cfg_json = serde_json::to_string(&cfg).unwrap();
-    println!("Config:\n{}", cfg_json);
-
+    info!("Config:\n{}", cfg_json);
 
     println!("{}", get_clipboard_conts());
 }
