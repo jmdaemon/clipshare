1d3e2c5d8762c9d5114f8a80a054c4e3676d69bb
Joseph Diza <josephm.diza@gmail.com>
2023-01-02 15:56:54 -0800

    Add clipboard to Device

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ extern crate pretty_env_logger;
 pub struct Device {
     name: String,
     history: Vec<String>,
+    clipboard: ClipboardContext,
+}
+
+impl Device {
+    pub fn new(name: String, history: Vec<String>, clipboard: ClipboardContext) -> Device {
+        Device { name, history, clipboard }
+    }
 }
 
 type Shortcuts = HashMap<String, String>;
@@ -46,12 +53,20 @@ impl Config {
 fn main() {
     pretty_env_logger::init();
 
+    let dev_ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let dev = Device::new(String::from("Device 1"), vec![String::from("asdf")], dev_ctx);
+    info!("Device Name: {}", dev.name);
+    info!("Device History:");
+    dev.history.iter().for_each( |line| {
+        info!("{}", line);
+    });
+
     let cfg = Config::default();
     let cfg_json = serde_json::to_string(&cfg).unwrap();
     info!("Config:\n{}", cfg_json);
 
 
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let mut ctx = dev.clipboard;
     println!("{}", get_clipboard_conts(&mut ctx));
 
     set_clipboard_conts(&mut ctx, String::from("saved-to-clipboard"));
