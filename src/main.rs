91ec17a0e351bcdfa20376ebbd48c33373a125b0
Joseph Diza <josephm.diza@gmail.com>
2023-01-02 13:15:04 -0800

    Add clipboard example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate clipboard;
+
+use clipboard::ClipboardProvider;
+use clipboard::ClipboardContext;
+
+fn example() {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    println!("{:?}", ctx.get_contents());
+    ctx.set_contents("some string".to_owned()).unwrap();
+}
+
 fn main() {
-    println!("Hello, world!");
+    example();
 }
