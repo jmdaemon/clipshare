1d02c5eca62639f7338ce5c4b243e68b715422bb
Joseph Diza <josephm.diza@gmail.com>
2023-01-02 16:20:52 -0800

    Split clipshare into bin/lib

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,11 @@
-// Standard Library
-use std::collections::HashMap;
+// Imports
+use clipshare::{Device, Config, get_clipboard_conts, set_clipboard_conts};
 
 // Third Party Libraries
 use clipboard::{ClipboardContext, ClipboardProvider};
-use serde::{Serialize, Deserialize};
 
 extern crate pretty_env_logger;
 #[macro_use] extern crate log;
-
-// Data Structures
-pub struct Device {
-    name: String,
-    history: Vec<String>,
-    clipboard: ClipboardContext,
-}
-
-impl Device {
-    pub fn new(name: String, history: Vec<String>, clipboard: ClipboardContext) -> Device {
-        Device { name, history, clipboard }
-    }
-}
-
-type Shortcuts = HashMap<String, String>;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
-    max_history: u64,
-    shortcuts: Shortcuts,
-}
-
-// Clipboard
-pub fn get_clipboard_conts(ctx: &mut ClipboardContext) -> String {
-    ctx.get_contents().unwrap()
-}
-
-pub fn set_clipboard_conts(ctx: &mut ClipboardContext, conts: String) {
-    ctx.set_contents(conts).expect("Could not set contents of clipboard");
-}
-
-impl Config {
-    pub fn default() -> Config {
-        let shortcuts = HashMap::from([
-                (String::from("Enable/Disable Device"), String::from("Ctrl + {}")),
-        ]);
-        Config { max_history:10_000, shortcuts}
-    }
-    pub fn new(&self, max_history: u64, shortcuts: Shortcuts) -> Config {
-        Config { max_history, shortcuts }
-    }
-}
 
 fn main() {
     pretty_env_logger::init();
