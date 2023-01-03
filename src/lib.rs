c8c679c7cf44c54e73d09d4acd9a6c13c7d51844
Joseph Diza <josephm.diza@gmail.com>
2023-01-02 16:28:12 -0800

    Create mem fns {set,get}_clipboard_conts for Device

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-// Standard Library
 use std::collections::HashMap;
 
-// Third Party Libraries
 use clipboard::{ClipboardContext, ClipboardProvider};
 use serde::{Serialize, Deserialize};
+
+#[macro_use] extern crate log;
 
 // Data Structures
 pub struct Device {
@@ -16,6 +16,14 @@ impl Device {
     pub fn new(name: String, history: Vec<String>, clipboard: ClipboardContext) -> Device {
         Device { name, history, clipboard }
     }
+
+    pub fn get_clipboard_conts(&mut self) -> String {
+        get_clipboard_conts(&mut self.clipboard)
+    }
+
+    pub fn set_clipboard_conts(&mut self, conts: String) {
+        set_clipboard_conts(&mut self.clipboard, conts)
+    }
 }
 
 type Shortcuts = HashMap<String, String>;
@@ -27,11 +35,11 @@ pub struct Config {
 }
 
 // Clipboard
-pub fn get_clipboard_conts(ctx: &mut ClipboardContext) -> String {
+fn get_clipboard_conts(ctx: &mut ClipboardContext) -> String {
     ctx.get_contents().unwrap()
 }
 
-pub fn set_clipboard_conts(ctx: &mut ClipboardContext, conts: String) {
+fn set_clipboard_conts(ctx: &mut ClipboardContext, conts: String) {
     ctx.set_contents(conts).expect("Could not set contents of clipboard");
 }
 
@@ -45,4 +53,15 @@ impl Config {
     pub fn new(&self, max_history: u64, shortcuts: Shortcuts) -> Config {
         Config { max_history, shortcuts }
     }
+}
+
+pub fn init_device() -> Device {
+    let dev_ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let dev = Device::new(String::from("Device 1"), vec![String::from("asdf")], dev_ctx);
+    info!("Device Name: {}", dev.name);
+    info!("Device History:");
+    dev.history.iter().for_each( |line| {
+        info!("{}", line);
+    });
+    dev
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
-// Imports
-use clipshare::{Device, Config, get_clipboard_conts, set_clipboard_conts};
+use clipshare::{init_device, Config};
 
-// Third Party Libraries
-use clipboard::{ClipboardContext, ClipboardProvider};
-
-extern crate pretty_env_logger;
 #[macro_use] extern crate log;
 
 fn main() {
     pretty_env_logger::init();
 
-    let dev_ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    let dev = Device::new(String::from("Device 1"), vec![String::from("asdf")], dev_ctx);
-    info!("Device Name: {}", dev.name);
-    info!("Device History:");
-    dev.history.iter().for_each( |line| {
-        info!("{}", line);
-    });
-
     let cfg = Config::default();
     let cfg_json = serde_json::to_string(&cfg).unwrap();
     info!("Config:\n{}", cfg_json);
 
+    let mut dev = init_device();
 
-    let mut ctx = dev.clipboard;
-    println!("{}", get_clipboard_conts(&mut ctx));
+    println!("{}", dev.get_clipboard_conts());
 
-    set_clipboard_conts(&mut ctx, String::from("saved-to-clipboard"));
-    println!("{}", get_clipboard_conts(&mut ctx));
+    dev.set_clipboard_conts(String::from("saved-to-clipboard"));
+    println!("{}", dev.get_clipboard_conts());
 }
