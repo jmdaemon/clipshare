ba9f3a07bf6a2cb4b1f3c498634fe67f52a1b0da
Joseph Diza <josephm.diza@gmail.com>
2023-01-06 08:05:32 -0800

    Join multiple client tasks together on one thread

--- src/bin/async.rs
+++ src/bin/async.rs
@@ -1,21 +1,17 @@
 use std::{
     collections::HashMap,
     env,
-    io::Error as IoError,
     net::SocketAddr,
     sync::{Arc, Mutex},
     thread, vec,
 };
 
-use futures::{channel::mpsc::{unbounded, UnboundedSender}, stream::{SplitStream, ForEach}, TryFutureExt};
-use futures::{channel, future, pin_mut, stream::TryStreamExt, StreamExt};
-
-//use tokio::sync::broadcast;
-//use tokio::sync::broadcast::error::{SendError, RecvError};
-//use tokio::sync::broadcast::{Sender, Receiver};
-//use tokio::time::Duration;
-//use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
+use futures::{
+    channel, future, pin_mut, StreamExt,
+    channel::mpsc::{unbounded, UnboundedSender},
+    stream::{TryStreamExt}, TryFutureExt,
+    future::join_all,
+};
 use tokio::{
     net::{TcpListener, TcpStream},
     io::{AsyncReadExt, AsyncWriteExt},
@@ -23,18 +19,10 @@ use tokio::{
     sync::broadcast::{Sender, Receiver, error::{SendError, RecvError}},
     time::Duration,
 };
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message, WebSocketStream};
-
-//use tokio::net::{TcpListener, TcpStream};
-//use tokio::stream::SplitStream;
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
 type Tx = UnboundedSender<Message>;
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
-
-
-// Execution model
-// We send clipboard events over this channel
-// we also listen for clipboard events over this channel
 
 #[derive(Debug, Clone)]
 pub enum ClipboardEvent {
@@ -69,10 +57,8 @@ pub async fn cb_receive_update(r: &mut Receiver<ClipboardEvent>) {
     }
 }
 
-// Setup 
+/// Listen for client connections
 async fn setup_server(addr: String) -> TcpListener {
-
-    // Create the event loop and TCP listener we'll accept connections on.
     let try_socket = TcpListener::bind(addr.clone()).await;
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", addr);
@@ -132,20 +118,6 @@ async fn poll_client_connections(srv: TcpListener, state: PeerMap) {
     }
 }
 
-//async fn cb_client_message_received(read: SplitStream<WebSocketStream<TcpStream>>, message: Message) -> Result<Message, Error> {
-//async fn cb_client_message_received(read: SplitStream<WebSocketStream<TcpStream>>, message: Message) {
-//async fn cb_client_message_received(read: SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<TcpStream>>>) {
-
-//async fn cb_client_message_received(read: SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<TcpStream>>>) -> ForEach<SplitStream<WebSocketStream<TcpStream>>>{
-    //let ws_to_stdout = {
-        //read.for_each(|message| async {
-            //let data = message.unwrap().into_data();
-            //tokio::io::stdout().write_all(&data).await.unwrap();
-        //})
-    //};
-    //ws_to_stdout
-//}
-
 async fn setup_client(connect_addr: String) {
     let url = url::Url::parse(&connect_addr).unwrap();
 
@@ -158,20 +130,15 @@ async fn setup_client(connect_addr: String) {
     let (write, read) = ws_stream.split();
 
     let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-    //let ws_to_stdout = cb_client_message_received(read);
     let ws_to_stdout = {
         read.for_each(|message| async {
             let data = message.unwrap().into_data();
             tokio::io::stdout().write_all(&data).await.unwrap();
         })
     };
-    //let ws_to_stdout = {
-        //cb_client_message_received(message)
-    //};
 
     pin_mut!(stdin_to_ws, ws_to_stdout);
     future::select(stdin_to_ws, ws_to_stdout).await;
-
 }
 
 // Our helper method which will read data from stdin and send it along the
@@ -201,34 +168,38 @@ async fn main() {
 
     //let last_copied = "string-to-be-copied";
 
-
     // We want to set up our devices 
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-    //let connect_addr = addr.clone();
     let server = setup_server(addr.clone());
 
-    //let mut clients = vec![];
-    //for _ in 0..3 {
-        //clients.push(setup_client(&addr));
-    //}
+    let mut clients = vec![];
+    for i in 0..3 {
+        println!("Setup client #{}", i+1);
+        let url = format!("ws://{}", addr.clone());
+        clients.push(setup_client(url));
+    }
     //for _ in 0..3 {
         //setup_client(&addr).await;
     //}
-    let client_handles = tokio::spawn(
-        async move {
-            for i in 0..3 {
-                println!("Setup client #{}", i);
-                let url = format!("ws://{}", addr.clone());
-                setup_client(url).await;
-            }
-        }).unwrap_or_else(|_| {});
+    //let client_handles = tokio::spawn(
+        //async move {
+            //for i in 0..3 {
+                //println!("Setup client #{}", i);
+                //let url = format!("ws://{}", addr.clone());
+                //setup_client(url).await;
+            //}
+        //}).unwrap_or_else(|_| {});
     
     //server.await;
+    //let a = clients.get_mut(0).unwrap();
+    //let a = clients.get_mut(0);
     let state = PeerMap::new(Mutex::new(HashMap::new()));
     tokio::join!(
+        join_all(clients),
         poll_client_connections(server.await, state),
-        client_handles,
+        //client_handles,
         //&clients[0],
+        
 
         );
 
