899068f940e3ef45a234493ead4ba6c3ea34a2b0
Joseph Diza <josephm.diza@gmail.com>
2023-02-21 13:27:12 -0800

    Add client, server binaries for testing

---
+++ src/bin/client.rs
@@ -0,0 +1,10 @@
+use clipshare::ws::setup_client;
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    let url = format!("ws://{}", addr.clone());
+    let client = setup_client(url);
+    tokio::join!(client);
+}
