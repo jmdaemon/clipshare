a88e342ee124da284872cf25c767588a621a26ba
Joseph Diza <josephm.diza@gmail.com>
2023-03-01 07:53:04 -0800

    Create initial user interface

---
+++ src/bin/ui.rs
@@ -0,0 +1,11 @@
+use clipshare::{
+    widgets::app::{App, AppInit},
+    consts::APP_ID
+};
+use relm4::RelmApp;
+
+fn main() {
+    let app = RelmApp::new(APP_ID);
+    let init = AppInit {};
+    app.run::<App>(init);
+}
---
+++ src/widgets/app.rs
@@ -0,0 +1,120 @@
+use gtk::prelude::{
+    ButtonExt,
+    GtkWindowExt,
+    OrientableExt,
+    WidgetExt,
+};
+
+use relm4::{
+    component,
+    gtk,
+    SimpleComponent,
+    ComponentSender,
+    ComponentParts,
+    RelmWidgetExt,
+};
+
+#[derive(Debug)]
+pub struct App {
+
+}
+
+#[derive(Debug)]
+pub enum AppMsg {}
+
+pub struct AppInit {}
+
+#[component(pub)]
+impl SimpleComponent for App {
+    type Input = ();
+    type Output = AppMsg;
+    type Init = AppInit;
+
+    view! {
+
+        gtk::Window {
+            set_title: Some("Clipshare"),
+            set_default_width: 640,
+            set_default_height: 480,
+
+            gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+                #[name="toolbar"]
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_margin_all: 5,
+
+                    gtk::Button {
+                        set_label: "Settings",
+                    },
+                    gtk::Button {
+                        set_label: "Shortcuts",
+                    },
+                },
+                #[name="mainview"]
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_margin_all: 5,
+                    set_hexpand: true,
+                    set_vexpand: true,
+
+                    gtk::StackSidebar {
+                        set_stack = &gtk::Stack {
+                            //set_size_request: (500, 500),
+                            set_hexpand: true,
+                            set_vexpand: true,
+
+                            // Home View
+                            add_child = &gtk::Box {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                set_margin_all: 5,
+                                set_size_request: (100, 100),
+                                set_hexpand: true,
+                                set_vexpand: true,
+
+
+                                gtk::Label {
+                                    set_label: "Main"
+                                },
+                            } -> {
+                                set_title: "Main",
+                                set_name: "Main",
+                            },
+
+                            // Example Device 1
+                            add_child = &gtk::ScrolledWindow {
+                                set_size_request: (300, 300),
+                                set_hexpand: true,
+                                set_vexpand: true,
+                                gtk::Label {
+                                    set_label: "Example Device 1",
+                                },
+                            } -> {
+                                set_title: "Example Device 1",
+                                set_name: "Example Device 1",
+                            },
+                        },
+                    },
+                }
+            }
+        }
+    }
+
+    fn init(
+        init: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+
+        let model = App {};
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
+        //match message {
+            //_ => todo!()
+        //}
+    }
+}
