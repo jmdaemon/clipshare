4d67f8d071f4daadf02d013851f3748dbc021d15
Joseph Diza <josephm.diza@gmail.com>
2023-03-04 16:18:54 -0800

    Fix server not registering service

--- src/bin/discover-server.rs
+++ src/bin/discover-server.rs
@@ -1,27 +1,33 @@
-use mdns_sd::{ServiceDaemon, ServiceEvent};
+use clipshare::discover::{SERVICE_TYPE};
+use mdns_sd::{ServiceDaemon, ServiceInfo};
+use std::collections::HashMap;
 
 #[tokio::main]
 pub async fn main() {
     // Create a daemon
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
 
-    // Browse for a service type.
-    let service_type = "_mdns-sd-my-test._udp.local.";
-    let receiver = mdns.browse(service_type).expect("Failed to browse");
+    // Create a service info.
+    let service_type = SERVICE_TYPE;
+    let instance_name = "my_instance";
+    let host_ipv4 = "192.168.1.12";
+    let host_name = "192.168.1.12.local.";
+    let port = 5200;
+    let properties = [("property_1", "test"), ("property_2", "1234")];
 
-    // Receive the browse events in sync or async. Here is
-    // an example of using a thread. Users can call `receiver.recv_async().await`
-    // if running in async environment.
-    //std::thread::spawn(move || {
-        while let Ok(event) = receiver.recv() {
-            match event {
-                ServiceEvent::ServiceResolved(info) => {
-                    println!("Resolved a new service: {}", info.get_fullname());
-                }
-                other_event => {
-                    println!("Received other event: {:?}", &other_event);
-                }
-            }
-        }
-    //});
+    let my_service = ServiceInfo::new(
+        service_type,
+        instance_name,
+        host_name,
+        host_ipv4,
+        port,
+        &properties[..],
+    ).unwrap();
+
+    // Register with the daemon, which publishes the service.
+    mdns.register(my_service).expect("Failed to register our service");
+    //while mdns.monitor() {
+        //mdns
+    //}
+    //mdns.monitor();
 }
