f203aaf8e1bb27c2c77811ec4f0b15cfef788f3b
Joseph Diza <josephm.diza@gmail.com>
2023-03-06 13:45:44 -0800

    Prune dead code

--- src/bin/app-client.rs
+++ src/bin/app-client.rs
@@ -1,12 +1,13 @@
 use clipshare::{ws::setup_client, discover::{DeviceMonitor, handle_new_client, SERVICE_TYPE}};
-use std::{env, net::Ipv4Addr};
+use std::net::Ipv4Addr;
 
 #[tokio::main]
 async fn main() {
+    let mut addr = Ipv4Addr::LOCALHOST;
+    let port = 5200;
+
     // Wait for device to be paired
     let device_monitor = DeviceMonitor::new(SERVICE_TYPE);
-
-    let mut addr = Ipv4Addr::LOCALHOST;
     while let Ok(event) = device_monitor.receiver.recv_async().await {
         if let Some(service_info) = handle_new_client(&event) { 
             let addresses = service_info.get_addresses();
@@ -21,15 +22,7 @@ async fn main() {
     println!("Connecting to device {}", addr);
 
     // Create client to the connected device
-    //let url = format!("ws://{}", addr);
-    //let url = format!("ws://{}:8080", addr);
-    //let url = format!("ws://{}:8080", addr);
-    //let url = format!("ws://{}.local:8080", addr);
-    //let url = format!("ws://{}", addr);
-    //let url = format!("ws://{}", addr);
-    //let url = format!("ws://{}:8080", addr);
-    let url = format!("ws://{}:5200", addr);
-    //let url = format!("ws://127.0.0.1:8080");
+    let url = format!("ws://{}:{}", addr, port);
     let client = setup_client(url);
     tokio::join!(client);
 }
--- src/bin/app-server.rs
+++ src/bin/app-server.rs
@@ -1,41 +1,36 @@
-use clipshare::ws::{setup_server, poll_client_connections};
+use clipshare::ws::{Address, setup_server, poll_client_connections};
 use clipshare::ws::PeerMap;
-use clipshare::discover::{Device, DeviceMonitor, SERVICE_TYPE, handle_new_client};
+use clipshare::discover::{Device, SERVICE_TYPE};
 
+use std::sync::Arc;
 use std::{
     thread::sleep,
     time::Duration,
-    env,
     sync::Mutex,
     collections::HashMap,
 };
+use local_ip_address::local_ip;
 
-pub async fn start_server() {
-    // Create server
-    //let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-    //let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
-    //let addr = env::args().nth(1).unwrap_or_else(|| "192.168.1.12:8080".to_string());
-    //let addr = env::args().nth(1).unwrap_or_else(|| "192.168.1.76:8080".to_string());
-    let addr = env::args().nth(1).unwrap_or_else(|| "192.168.1.76:5200".to_string());
+// Create server
+pub async fn start_server(address: Arc<Address>) {
+    let addr = address.to_string();
     let server = setup_server(addr.clone());
     let state = PeerMap::new(Mutex::new(HashMap::new()));
 
     tokio::join!(poll_client_connections(server.await, state));
 }
 
-pub async fn register_device() {
-    // Attempt to register device
+// Attempt to register device
+pub async fn register_device(address: Arc<Address>) {
     let service_type = SERVICE_TYPE;
     let instance_name = "my_instance";
-    //let host_ipv4 = "192.168.1.12";
-    //let host_name = "192.168.1.12.local.";
-    let host_ipv4 = "192.168.1.76";
-    let host_name = "192.168.1.76.local.";
+    let addr = &address.ip;
+    let host_ipv4 = addr;
+    let host_name = format!("{}.local.", host_ipv4);
     let port = 5200;
     let properties = [("property_1", "test"), ("property_2", "1234")];
     
-    let device = Device::new(service_type, instance_name, host_ipv4, host_name, port, &properties[..]);
-    //device.register_device().await;
+    let device = Device::new(service_type, instance_name, host_ipv4, &host_name, port, &properties[..]);
     if let Some(service) = &device.service_info {
         // TODO: Run forever for now, we'll kill this later somehow,
         // when we drop the connection.
@@ -55,11 +50,20 @@ pub async fn register_device() {
 
 #[tokio::main]
 pub async fn main() {
-    let thread_register_device = tokio::spawn(async {
-        register_device().await
+    let ip = local_ip().unwrap().to_string();
+    let port = 5200;
+
+    let addr = Address { ip, port };
+    let addr = Arc::new(addr);
+
+    let addr_reg_dev = Arc::clone(&addr);
+    let thread_register_device = tokio::spawn(async move {
+        register_device(addr_reg_dev).await
     });
-    let thread_start_server = tokio::spawn(async {
-        start_server().await
+
+    let addr_start_serv = Arc::clone(&addr);
+    let thread_start_server = tokio::spawn(async move {
+        start_server(addr_start_serv).await
     });
     // In another thread, establish the server
     tokio::join!(
