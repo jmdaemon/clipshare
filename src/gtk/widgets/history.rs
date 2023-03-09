33b7eb98964f75a1c6ca8f03014824d54c45ea2b
Joseph Diza <josephm.diza@gmail.com>
2023-03-09 12:05:01 -0800

    Implement outline for basic history widget

--- src/gtk/widgets/history.rs
+++ src/gtk/widgets/history.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use gtk::prelude::{
     ButtonExt,
     BoxExt,
@@ -6,52 +8,60 @@ use gtk::prelude::{
 };
 
 use relm4::{
-    factory::FactoryView,
+    factory::{FactoryView, FactoryVecDeque},
     gtk,
     prelude::{DynamicIndex, FactoryComponent},
-    FactorySender,
+    ComponentSender,
+    ComponentParts,
+    FactorySender, SimpleComponent,
 };
 
-pub struct HistoryModel {
+#[derive(Debug)]
+pub struct HistoryEntry {
     pub last_copied: String,
 }
 
 #[derive(Debug)]
-pub enum HistoryInput {}
+pub enum HistoryEntryInput {}
 
 #[derive(Debug)]
-pub enum HistoryOutput {}
+pub enum HistoryEntryOutput {}
 
-pub struct HistoryInit {}
+pub struct HistoryEntryInit {}
 
 #[relm4::factory(pub)]
-impl FactoryComponent for HistoryModel {
-    type ParentWidget = gtk::Box;
-    type ParentInput = ();
-    type Input = HistoryInput;
-    type Output = HistoryOutput;
-    type Init = String;
+impl FactoryComponent for HistoryEntry {
+    //type Init = String;
+    type Init = HistoryEntry;
+    type Input = HistoryEntryInput;
+    //type Input = HistoryEntryInput;
+    type Output = HistoryEntryOutput;
     type CommandOutput = ();
+    type ParentInput = HistoryInput;
+    //type ParentWidget = gtk::Box;
+    type ParentWidget = gtk::ListBox;
 
     view! {
         #[root]
-        gtk::Box {
-            set_orientation: gtk::Orientation::Horizontal,
-            set_spacing: 12,
+        gtk::ListBox {
+            gtk::Box {
+                set_orientation: gtk::Orientation::Horizontal,
+                set_spacing: 12,
 
-            gtk::Label {
-                #[watch]
-                set_label: &self.last_copied,
-                set_width_chars: 64,
-            },
-            gtk::Button {
-                set_height_request: 24,
-                set_label: "Copy"
-            },
-            gtk::Button {
-                set_height_request: 24,
-                set_label: "Delete"
-            },
+                gtk::Label {
+                    #[watch]
+                    set_label: &self.last_copied,
+                    set_width_chars: 64,
+                },
+                gtk::Button {
+                    set_height_request: 24,
+                    set_label: "Copy"
+                },
+                gtk::Button {
+                    set_height_request: 24,
+                    set_label: "Delete"
+                },
+            }
         }
     }
 
@@ -60,9 +70,10 @@ impl FactoryComponent for HistoryModel {
         index: &DynamicIndex,
         sender: FactorySender<Self>,
     ) -> Self {
-        Self {
-            last_copied: init
-        }
+        init
+        //Self {
+            //last_copied: init
+        //}
     }
 
     fn init_widgets(
@@ -83,5 +94,86 @@ impl FactoryComponent for HistoryModel {
     fn output_to_parent_input(output: Self::Output) -> Option<Self::ParentInput> {
         let output = match output {};
         Some(output)
+    }
+}
+
+
+#[derive(Debug)]
+pub struct HistoryModel {
+    //pub copied: VecDeque<String>,
+    //pub copied: VecDeque<String>,
+    pub history: FactoryVecDeque<HistoryEntry>,
+}
+
+#[derive(Debug)]
+pub enum HistoryInput {}
+
+#[derive(Debug)]
+pub enum HistoryOutput {}
+
+//pub struct HistoryInit {}
+
+#[relm4::component(pub)]
+impl SimpleComponent for HistoryModel {
+    type Input = HistoryInput;
+    type Output = HistoryOutput;
+    //type Init = HistoryInit;
+    type Init = ();
+
+    view! {
+        #[root]
+        gtk::ScrolledWindow {
+            //gtk::Box {
+                //set_orientation: gtk::Orientation::Vertical,
+            gtk::ListBox {
+                #[local_ref]
+                history_box -> gtk::ListBox {
+                    //set_orientation: gtk::Orientation::Horizontal,
+                    //set_vexpand: true,
+                    set_hexpand: true,
+                }
+                //#[local_ref]
+                //history_box -> gtk::Box {
+                    ////set_orientation: gtk::Orientation::Horizontal,
+                    ////set_vexpand: true,
+                    //set_hexpand: true,
+                //}
+            }
+        }
+    }
+
+    fn init(
+        init: Self::Init,
+        root: &Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+
+        //let mut history = FactoryVecDeque::new(gtk::Box::default(), sender.input_sender());
+        let mut history = FactoryVecDeque::new(gtk::ListBox::default(), sender.input_sender());
+
+        // Test out adding a few entries
+        let clipboard = vec![
+            "This".to_owned(),
+            "Will".to_owned(),
+            "Be".to_owned(),
+            "Copied".to_owned(),
+        ];
+
+        clipboard.into_iter().for_each(|line| {
+            let entry = HistoryEntry { last_copied: line };
+            history.guard().push_back(entry);
+        });
+
+        let model = HistoryModel { history };
+        let history_box = model.history.widget();
+
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
+        match message {
+
+        }
     }
 }
