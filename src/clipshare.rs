5be24ac5a7a979d4c572bc71bdbc5384edc3f842
Joseph Diza <josephm.diza@gmail.com>
2023-03-14 12:10:39 -0700

    Implement Server data structure

--- src/clipshare.rs
+++ src/clipshare.rs
@@ -1,3 +1,4 @@
+// Client Imports
 use crate::{
     config::{Settings, SettingsBuilder},
     device::init_device,
@@ -5,10 +6,22 @@ use crate::{
     ws::{Dev, Address, setup_client, AddressBuilder},
 };
 
-use std::{
-    sync::{Arc, Mutex},
-    net::Ipv4Addr
+use std::sync::{Arc, Mutex};
+
+// Server Imports
+use crate::{
+    discover::Device,
+    ws::{PeerMap, poll_client_connections, setup_server}
 };
+
+use std::{
+    net::Ipv4Addr,
+    thread::sleep,
+    time::Duration,
+    collections::HashMap,
+};
+
+use local_ip_address::local_ip;
 
 pub struct Client {
     pub settings: Settings,
@@ -18,14 +31,17 @@ pub struct Client {
 
 pub struct Server {
     pub settings: Settings,
+    pub device: Dev,
+    pub address: Arc<Address>,
 }
 
+// Client
 impl Default for Client {
     fn default() -> Self {
         let settings = SettingsBuilder::new().build();
         let device = Arc::new(Mutex::new(init_device()));
         let address = AddressBuilder::new().build();
-        Client { settings, device, address }
+        Self { settings, device, address }
     }
 }
 
@@ -64,5 +80,84 @@ impl Client {
         //let client = setup_client(url);
         let client = setup_client(self.device.to_owned(), url);
         tokio::join!(client);
+    }
+}
+
+// Server
+async fn start_server(dev: Dev, address: Arc<Address>) {
+    //let addr = address.to_string();
+    //let server = setup_server(addr.clone());
+    let server = setup_server(address.to_string());
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+    tokio::join!(poll_client_connections(dev, server.await, state));
+}
+
+// Attempt to register device
+async fn register_device(address: Arc<Address>) {
+    let service_type = SERVICE_TYPE;
+    let instance_name = "my_instance";
+    let addr = &address.ip;
+    let host_ipv4 = addr;
+    let host_name = format!("{}.local.", host_ipv4);
+    let port = address.port as u16; // FIXME: Make sure to change port data type
+    let properties = [("property_1", "test"), ("property_2", "1234")];
+    
+    let device = Device::new(service_type, instance_name, host_ipv4, &host_name, port, &properties[..]);
+    if let Some(service) = &device.service_info {
+        // TODO: Run forever for now, we'll kill this later somehow,
+        // when we drop the connection.
+        loop {
+            // Register with the daemon, which publishes the service.
+            device.mdns.register(service.to_owned()).expect("Failed to register our service");
+            sleep(Duration::from_secs(15));
+
+            //let results = device.mdns.get_metrics().unwrap().into_recv_async().await.unwrap();
+            //results.iter().for_each(|(k, v)| {
+                //println!("Metrics Key: {}\nMetrics Value: {}", k, v);
+            //});
+        }
+    }
+    
+}
+
+impl Default for Server {
+    fn default() -> Self {
+        let settings = SettingsBuilder::new().build();
+        let device = Arc::new(Mutex::new(init_device()));
+        let address = Arc::new(
+            AddressBuilder::new()
+            .ip(local_ip().unwrap().to_string())
+            .build());
+        Self { settings, device, address }
+    }
+}
+
+
+impl Server {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub async fn start(&self) {
+        let addr_register_device = self.address.clone();
+        let addr_websocket_server = self.address.clone();
+
+        // Create async task
+        let thread_register_device = tokio::spawn(async move {
+            register_device(addr_register_device).await
+        });
+
+        // In another thread, establish the server
+        let device = self.device.clone();
+        let thread_start_server = tokio::spawn(async move {
+            start_server(device, addr_websocket_server).await
+        });
+
+        let joined = tokio::join!(
+            thread_register_device,
+            thread_start_server,
+        );
+        joined.0.expect("Could not register device");
+        joined.1.expect("Could not start server");
     }
 }
