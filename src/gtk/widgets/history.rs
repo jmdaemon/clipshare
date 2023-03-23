369e55cdc770e0086e5d6094c202e65f071eb94a
Joseph Diza <josephm.diza@gmail.com>
2023-03-23 16:32:10 -0700

    Implement delete button for history widget

--- src/gtk/widgets/history.rs
+++ src/gtk/widgets/history.rs
@@ -5,10 +5,6 @@ use gtk::prelude::{
     WidgetExt,
     //FrameExt,
 };
-//use gdk4::{
-    //Cursor::
-    
-//};
 
 use relm4::{
     factory::{FactoryView, FactoryVecDeque},
@@ -27,62 +23,80 @@ pub struct HistoryEntry {
     pub last_copied: String,
 }
 
-#[derive(Debug)]
-pub enum HistoryEntryInput {}
-
-#[derive(Debug)]
-pub enum HistoryEntryOutput {}
-
-pub struct HistoryEntryInit {}
-
-//pub fn index(i: DynamicIndex) -> String {
-    //String::from(self.index.current_index() + 1)
+//#[derive(Debug)]
+//pub enum HistoryEntryInput {
+    //AddEntry,
 //}
+
+#[derive(Debug)]
+pub enum HistoryEntryOutput {
+    CopyEntry(DynamicIndex),
+    DeleteEntry(DynamicIndex),
+}
 
 #[relm4::factory(pub)]
 impl FactoryComponent for HistoryEntry {
     type Init = String;
-    type Input = HistoryEntryInput;
+    //type Input = HistoryEntryInput;
+    type Input = ();
     type Output = HistoryEntryOutput;
     type CommandOutput = ();
     type ParentInput = HistoryInput;
-    //type ParentWidget = gtk::Box;
     type ParentWidget = gtk::ListBox;
 
     view! {
         #[root]
-        gtk::Box {
+        root = gtk::Box {
             set_orientation: gtk::Orientation::Horizontal,
             set_spacing: 12,
             set_hexpand: true,
-                gtk::Label {
-                    #[watch]
-                    set_label: &(self.index.current_index() + 1).to_string(),
-                    set_width_chars: 8,
-                    set_xalign: 0.6,
-                },
 
-                gtk::Label {
-                    set_css_classes: &["clipboard-entry"],
-                    set_can_target: true,
-
-                    #[watch]
-                    set_label: &self.last_copied,
-                    set_width_chars: 128,
-                    set_xalign: 0.00,
-                    //connect_cursor_notify[sender] => move |_| {
-                        //println!("Changed cursor");
-                        //gtk::Widget::set_cursor(&self, gdk::Cursor::);
-                        //gtk::Window::new().set_cursor(gtk::Widget::set_cursor(&self, cursor));
-                    //}
-                },
-            gtk::Button {
-                set_height_request: 24,
-                set_label: "Copy"
+            #[name(lbl_index)]
+            gtk::Label {
+                #[watch]
+                set_label: &(self.index.current_index() + 1).to_string(),
+                set_width_chars: 8,
+                set_xalign: 0.6,
             },
+
+            #[name(lbl_history_entry)]
+            gtk::Label {
+                set_css_classes: &["clipboard-entry"],
+                set_can_target: true,
+
+                #[watch]
+                set_label: &self.last_copied,
+                set_width_chars: 128,
+                set_xalign: 0.00,
+                //connect_cursor_notify[sender] => move |_| {
+                    //println!("Changed cursor");
+                    //gtk::Widget::set_cursor(&self, gdk::Cursor::);
+                    //gtk::Window::new().set_cursor(gtk::Widget::set_cursor(&self, cursor));
+                //}
+            },
+
+            #[name(btn_copy)]
             gtk::Button {
                 set_height_request: 24,
-                set_label: "Delete"
+                set_label: "Copy",
+                connect_clicked[sender, index] => move |_| {
+                    sender.output(HistoryEntryOutput::CopyEntry(index.clone()))
+                }
+            },
+
+            #[name(btn_delete)]
+            gtk::Button {
+                set_height_request: 24,
+                set_label: "Delete",
+                connect_clicked[sender, index] => move |_| {
+                    sender.output(HistoryEntryOutput::DeleteEntry(index.clone()))
+                }
+                //connect_clicked[sender, index] => move |_| {
+                    //let index = self.index.current_index();
+                    //sender.output(HistoryInput::DeleteEntry(index));
+                    //sender.output(HistoryInput::DeleteEntry(0));
+                    //sender.input(HistoryEntryInput::DeleteEntry);
+                //}
             },
         }
     }
@@ -109,16 +123,41 @@ impl FactoryComponent for HistoryEntry {
         widgets
     }
 
-    fn update(&mut self, message: Self::Input, sender: FactorySender<Self>) {
-        match message {}
+    fn output_to_parent_input(output: Self::Output) -> Option<HistoryInput> {
+        Some(match output {
+            HistoryEntryOutput::DeleteEntry(index) => HistoryInput::DeleteEntry(index),
+            HistoryEntryOutput::CopyEntry(index) => HistoryInput::CopyEntry(index),
+        })
     }
 
+    fn update(&mut self, message: Self::Input, sender: FactorySender<Self>) {
+        //match message {
+            //HistoryEntryInput::AddEntry => {
+
+            //},
+
+            /*
+            HistoryEntryInput::CopyEntry => {
+                // Copy the clipboard to the current device contents
+            }
+            HistoryEntryInput::DeleteEntry => {
+                // Send the index to the main factory widget
+                // In the main factory widget, delete the current entry
+                // Set the current index to the index of the deleted widget?
+                let index = self.index.current_index();
+                sender.output(HistoryInput::DeleteEntry(index));
+            }
+            */
+        //}
+    }
+
+    /*
     fn output_to_parent_input(output: Self::Output) -> Option<Self::ParentInput> {
         let output = match output {};
         Some(output)
     }
+    */
 }
-
 
 #[derive(Debug)]
 pub struct HistoryModel {
@@ -126,7 +165,10 @@ pub struct HistoryModel {
 }
 
 #[derive(Debug)]
-pub enum HistoryInput {}
+pub enum HistoryInput {
+    CopyEntry(DynamicIndex),
+    DeleteEntry(DynamicIndex),
+}
 
 #[derive(Debug)]
 pub enum HistoryOutput {}
@@ -140,10 +182,8 @@ impl SimpleComponent for HistoryModel {
     view! {
         #[root]
         gtk::ScrolledWindow {
-            //#[name="lb_entries"]
             gtk::ListBox {
                 #[local_ref]
-                //history_box -> gtk::Box,
                 history_box -> gtk::ListBox,
             }
         }
@@ -156,7 +196,6 @@ impl SimpleComponent for HistoryModel {
     ) -> ComponentParts<Self> {
 
         let mut history = FactoryVecDeque::new(gtk::ListBox::default(), sender.input_sender());
-        //let mut history = FactoryVecDeque::new(gtk::Box::default(), sender.input_sender());
 
         // Test out adding a few entries
         let clipboard = vec![
@@ -166,32 +205,10 @@ impl SimpleComponent for HistoryModel {
             "Copied".to_owned(),
         ];
 
-        //let mut index: usize = 1;
-        //clipboard.into_iter().for_each(|line| {
-        //let history = clipboard.into_iter().for_each(|line| {
         clipboard.into_iter().for_each(|line| {
-            //history.guard().push_front(line);
-            //history.guard().push_back(line);
-
-            /*
-            let len = history.len();
-            let last = history.get(len);
-            if let Some(last) = last {
-                history.guard().swap(len, 0);
-                //last.index.to_owned + 1;
-            }
-            */
-
             let index = history.guard().push_back(line);
             history.guard().move_front(index.current_index());
-
-            //let entry = HistoryEntry { index, last_copied: line };
-            //history.guard().push_front(entry);
-            //index += 1;
         });
-        //}).rev().collect();
-        //history.iter().rev()
-        //let history = history.iter().map(|entry: &HistoryEntry| entry.to_owned()).rev().collect();
 
         let model = HistoryModel { history };
 
@@ -223,7 +240,27 @@ impl SimpleComponent for HistoryModel {
 
     fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
         match message {
+            HistoryInput::DeleteEntry(index) => {
+                println!("Received DeleteEntry");
+                self.history.guard().remove(index.current_index());
 
+                //let child = self.history.get(position).unwrap().init_root();
+                //let child = self.history.get(position).unwrap().init_root();
+                //self.history.widget().remove(&child);
+                
+                //let last = self.history.len();
+                //self.history.guard().pop_back();
+                //self.history.widget().remove(child)
+
+                /*
+                self.history.guard().move_to(position, last);
+                //self.history.guard().move_to(position, 0);
+                self.history.guard().pop_back();
+                */
+            }
+            HistoryInput::CopyEntry(index) => {
+                // Copy the thing
+            }
         }
     }
 }
