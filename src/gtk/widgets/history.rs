3658ea5cdfd23f130ea7ab9f6b9ab8ce94f77fbe
Joseph Diza <josephm.diza@gmail.com>
2023-03-23 19:50:56 -0700

    Implement index updating for history widget
    This may not be a good idea as it may be more ideal to want the widgets
    to keep their current ids instead. This also requires the previous
    ordering of items to break. In the future implementing a proper
    container or index type is more ideal.

--- src/gtk/widgets/history.rs
+++ src/gtk/widgets/history.rs
@@ -1,3 +1,5 @@
+use std::ops::{Sub, SubAssign};
+
 use gtk::prelude::{
     BoxExt,
     ButtonExt,
@@ -23,10 +25,12 @@ pub struct HistoryEntry {
     pub last_copied: String,
 }
 
-//#[derive(Debug)]
-//pub enum HistoryEntryInput {
-    //AddEntry,
-//}
+#[derive(Debug, Clone)]
+pub enum HistoryEntryInput {
+    //UpdateIndex(DynamicIndex),
+    DecrementIndex,
+    RefreshIndex,
+}
 
 #[derive(Debug)]
 pub enum HistoryEntryOutput {
@@ -37,8 +41,7 @@ pub enum HistoryEntryOutput {
 #[relm4::factory(pub)]
 impl FactoryComponent for HistoryEntry {
     type Init = String;
-    //type Input = HistoryEntryInput;
-    type Input = ();
+    type Input = HistoryEntryInput;
     type Output = HistoryEntryOutput;
     type CommandOutput = ();
     type ParentInput = HistoryInput;
@@ -68,6 +71,8 @@ impl FactoryComponent for HistoryEntry {
                 set_label: &self.last_copied,
                 set_width_chars: 128,
                 set_xalign: 0.00,
+                //connect_clicked[sender] => move |_| {
+                //}
                 //connect_cursor_notify[sender] => move |_| {
                     //println!("Changed cursor");
                     //gtk::Widget::set_cursor(&self, gdk::Cursor::);
@@ -131,32 +136,22 @@ impl FactoryComponent for HistoryEntry {
     }
 
     fn update(&mut self, message: Self::Input, sender: FactorySender<Self>) {
-        //match message {
-            //HistoryEntryInput::AddEntry => {
-
-            //},
-
-            /*
-            HistoryEntryInput::CopyEntry => {
-                // Copy the clipboard to the current device contents
-            }
-            HistoryEntryInput::DeleteEntry => {
-                // Send the index to the main factory widget
-                // In the main factory widget, delete the current entry
-                // Set the current index to the index of the deleted widget?
-                let index = self.index.current_index();
-                sender.output(HistoryInput::DeleteEntry(index));
-            }
-            */
-        //}
+        match message {
+            HistoryEntryInput::DecrementIndex => {
+                if self.index.current_index() > 0 {
+                    self.index.current_index().sub_assign(1);
+                }
+                //self.index.current_index().sub(1);
+            },
+            HistoryEntryInput::RefreshIndex => {
+                self.index = self.index.to_owned();
+            },
+            //HistoryEntryInput::UpdateIndex(index) => {
+                //self.index = index;
+            //}
+        }
     }
 
-    /*
-    fn output_to_parent_input(output: Self::Output) -> Option<Self::ParentInput> {
-        let output = match output {};
-        Some(output)
-    }
-    */
 }
 
 #[derive(Debug)]
@@ -206,56 +201,63 @@ impl SimpleComponent for HistoryModel {
         ];
 
         clipboard.into_iter().for_each(|line| {
+            /*
             let index = history.guard().push_back(line);
             history.guard().move_front(index.current_index());
+            */
+            let index = history.guard().push_back(line);
         });
 
         let model = HistoryModel { history };
 
-        //model.history.iter() {
-        //}
         let history_box = model.history.widget();
         relm4::set_global_css_from_file("src/gtk/widgets/history.css");
 
         let widgets = view_output!();
-
-        //let append_entry = |lb_entries: &gtk::ListBox| {
-            //for entry in model.history.iter() {
-                //lb_entries.append(&entry.init_root());
-                ////let a = entry.init_root();
-                ////lb_entries.append(entry);
-            //}
-            //lb_entries
-        //};
-
-        //lb_entries = append_entry(&lb_entries).to_owned();
-        //for entry in model.history.iter() {
-            //lb_entries.append(&entry.init_root());
-            ////let a = entry.init_root();
-            ////lb_entries.append(entry);
-        //}
-        
+       
         ComponentParts { model, widgets }
     }
 
     fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
         match message {
             HistoryInput::DeleteEntry(index) => {
-                println!("Received DeleteEntry");
-                self.history.guard().remove(index.current_index());
+                println!("HistoryInput::DeleteEntry");
+                //let last = self.history.len() - 1;
+                //self.history.guard().move_back(index.current_index());
+                //self.history.guard().move_to(index.current_index(), last);
 
-                //let child = self.history.get(position).unwrap().init_root();
-                //let child = self.history.get(position).unwrap().init_root();
-                //self.history.widget().remove(&child);
-                
-                //let last = self.history.len();
-                //self.history.guard().pop_back();
-                //self.history.widget().remove(child)
+                //self.history.guard().move_to(index.current_index(), 0);
+                //self.history.guard().remove(last);
+                self.history.broadcast(HistoryEntryInput::RefreshIndex);
+                //self.history.broadcast(HistoryEntryInput::DecrementIndex);
+                //self.history.guard().remove(0);
+                self.history.guard().remove(index.current_index());
+                //self.history.guard().remove(last);
 
                 /*
-                self.history.guard().move_to(position, last);
-                //self.history.guard().move_to(position, 0);
-                self.history.guard().pop_back();
+                self.history.guard().remove(index.current_index());
+
+                // Ensure all the above entries are not affected
+                let last = self.history.len();
+                */
+                //for i in index.current_index() - 1..last {
+                //for i in index.current_index()..last {
+
+                /*
+                for i in index.current_index() - 1..last - 1 {
+                //for i in index.current_index() - 1..last - 2 {
+                    //if let Some(child) = self.history.get(i) {
+                    if let Some(child) = self.history.get(i) {
+                        //let new_index = DynamicIndex::from(i - 1);
+                        //child.update(HistoryEntryInput::UpdateIndex(new_index), sender)
+                        //child.update(HistoryEntryInput::UpdateIndex(i - 1), sender)
+                        //child.update(HistoryEntryInput::DecrementIndex, sender);
+
+                        //self.history.send(i, HistoryEntryInput::DecrementIndex);
+                        self.history.send(i, HistoryEntryInput::RefreshIndex);
+                    }
+                }
+            }
                 */
             }
             HistoryInput::CopyEntry(index) => {
