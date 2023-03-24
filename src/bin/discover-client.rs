84cc886e4310abc094b6d0099900a05f98372c09
Joseph Diza <josephm.diza@gmail.com>
2023-03-24 14:04:24 -0700

    Retrieve multiple device addresses

--- src/bin/discover-client.rs
+++ src/bin/discover-client.rs
@@ -1,7 +1,9 @@
 use clipshare::connect::discover::{SERVICE_TYPE, ServiceFinder};
+use std::time::Duration;
 
 #[tokio::main]
 pub async fn main() {
     let device_monitor = ServiceFinder::new(SERVICE_TYPE);
-    device_monitor.monitor_devices().await;
+    let timeout = Duration::from_secs(30);
+    device_monitor.find_devices(timeout).await;
 }
--- src/connect/client.rs
+++ src/connect/client.rs
@@ -5,7 +5,7 @@ use crate::{
     },
     connect::{
         address::{Address, AddressBuilder},
-        discover::{ServiceFinder, handle_new_client, SERVICE_TYPE},
+        discover::{ServiceFinder, SERVICE_TYPE},
         socket::setup_client,
     },
 };
@@ -39,21 +39,44 @@ impl Client {
     /// Scans for available devices in the LAN and obtains an ip address to the first
     /// available device. In the future, this should use an address pool, and a separate channel
     /// to manage addition/removal of devices
-    pub async fn monitor_devices(&self) -> Ipv4Addr {
-        let device_monitor = ServiceFinder::new(SERVICE_TYPE);
-        let mut device_addr = Ipv4Addr::UNSPECIFIED;
-        while let Ok(event) = device_monitor.receiver.recv_async().await {
-            if let Some(service_info) = handle_new_client(&event) { 
-                let addresses = service_info.get_addresses();
-                addresses.iter().for_each(|address| {
-                    device_addr = address.to_owned();
-                });
-                if device_addr != Ipv4Addr::UNSPECIFIED {
-                    break;
-                }
-            }
-        }
-        device_addr
+    pub async fn get_device_addresses(&self) -> Vec<Ipv4Addr> {
+
+        // Find devices with the service
+        let service_finder = ServiceFinder::new(SERVICE_TYPE);
+        let timeout = std::time::Duration::from_secs(30);
+        let found = service_finder.find_devices(timeout).await;
+
+        // Yield all the addresses
+        //let addresses: Vec<Ipv4Addr> = found.into_iter().map(|info| {
+        //info.get_addresses().into_iter()
+            //.map(|addr| addr.to_owned())
+            //.collect::<Vec<Ipv4Addr>>()
+        //}).flatten().collect();
+        //addresses
+
+        found.into_iter().flat_map(|info| {
+            info.get_addresses().iter()
+                .map(|addr| addr.to_owned())
+                .collect::<Vec<Ipv4Addr>>()
+        }).collect()
+
+
+        //}).fold(vec![], |acc, addresses| acc.push(addresses));
+        //addresses
+        //let mut first_address = Ipv4Addr::UNSPECIFIED;
+
+        //while let Ok(event) = service_finder.receiver.recv_async().await {
+            //if let Some(service_info) = handle_new_client(&event) { 
+                //let addresses = service_info.get_addresses();
+                //addresses.iter().for_each(|address| {
+                    //device_addr = address.to_owned();
+                //});
+                //if device_addr != Ipv4Addr::UNSPECIFIED {
+                    //break;
+                //}
+            //}
+        //}
+        //device_addr
     }
 
     /// Establishes the websocket connection to the device at address
