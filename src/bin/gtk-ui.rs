f4cedcfd4b6160c67b20e91434ec6d4d3d507896
Joseph Diza <josephm.diza@gmail.com>
2023-06-22 09:00:55 -0700

    Set global CSS file

--- src/bin/gtk-ui.rs
+++ src/bin/gtk-ui.rs
@@ -3,11 +3,11 @@ use clipshare::{
     ui::gtk::widgets::app::{App, AppInit},
     consts::APP_ID
 };
-use relm4::RelmApp;
+use relm4::{RelmApp, set_global_css_from_file};
 
 fn main() {
     pretty_env_logger::init();
-
+    set_global_css_from_file("src/gtk/clipshare.css");
     let app = RelmApp::new(APP_ID);
     let init = AppInit {};
     app.run::<App>(init);
